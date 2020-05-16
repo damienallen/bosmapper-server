8e2eaedfc40b9257bc981634d7568d971d2c18d3
Damien Allen <damien@mx3d.com>
2020-05-16 17:29:40 +0200

    Working docker setup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Tree(BaseModel):
+    species: str
+    lat: float
+    lon: float
+    notes: str = None
 
 
 @app.get("/")
@@ -8,6 +16,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/tree/add/")
+def add_tree(tree: Tree):
+    return tree
