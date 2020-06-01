d0fce690488305c38065c468366f98985257b534
Damien Allen <contact@dallen.co>
2020-06-01 02:12:49 +0200

    Added vector base

--- export/draw.py
+++ export/draw.py
@@ -11,14 +11,18 @@ current_dir = Path(__file__).resolve().parent
 # Constants
 DEFAULT_HEIGHT = 2
 DEFAULT_RADIUS = 4
-MARGIN = 10
-
-FILL_COLOR = [77 / 255, 115 / 255, 67 / 255]
-OUTLINE_COLOR = [54 / 255, 89 / 255, 62 / 255]
+MARGIN = 25
 
 
-def get_features():
-    geojson_path = current_dir / "data" / "20200530.geojson"
+def decimal_color(r, g, b):
+    return [r / 255, g / 255, b / 255]
+
+
+FILL_COLOR = decimal_color(77, 115, 67)
+OUTLINE_COLOR = decimal_color(54, 89, 62)
+
+
+def get_features(geojson_path):
     print(f"Parsing file {geojson_path}")
 
     with open(geojson_path, "r") as f:
@@ -32,6 +36,7 @@ def get_features():
 
 def extract_features(feature_list):
 
+    # TODO: calculate these from bounds
     lon_list = [feature["geometry"]["coordinates"][0] for feature in feature_list]
     lat_list = [feature["geometry"]["coordinates"][1] for feature in feature_list]
 
@@ -81,7 +86,7 @@ def extract_features(feature_list):
             )
 
     species_list = sorted(species_list, key=itemgetter("height"))
-    return (trees, species_list, scale_factor)
+    return (trees, species_list, scale_factor, min_lon, min_lat)
 
 
 def draw_background_outline(context, scale_factor, trees):
@@ -128,21 +133,86 @@ def draw_fills(context, scale_factor, trees, species_list):
     percent = (trees[0]["radius"] - min_radius) / (max_radius - min_radius)
 
     fill_color = fade_white(FILL_COLOR, percent)
-    print(fill_color)
 
     context.save()
 
     for tree in trees:
         context.arc(tree["x"], tree["y"], tree["radius"] - scale_factor / 20, 0, pi * 2)
-        context.set_source_rgba(fill_color[0], fill_color[1], fill_color[2], 1)
+        context.set_source_rgba(fill_color[0], fill_color[1], fill_color[2], 0.7)
         context.fill()
 
     context.restore()
 
 
+feature_styles = {
+    "boundary": {"stroke_color": decimal_color(179, 179, 179), "stroke_width": 0.5},
+    "bed": {"fill_color": decimal_color(230, 230, 230)},
+    "bee_hives": {"fill_color": decimal_color(179, 179, 179)},
+    "concrete": {"fill_color": decimal_color(250, 250, 250)},
+    "paved": {"fill_color": decimal_color(250, 250, 250)},
+    "greenhouse": {"fill_color": decimal_color(206, 220, 229)},
+    "misc": {"fill_color": decimal_color(203, 203, 203)},
+    "tree_ring": {"fill_color": decimal_color(203, 203, 203)},
+    "wall": {"fill_color": decimal_color(203, 203, 203)},
+    "vegetation": {
+        "stroke_color": decimal_color(203, 203, 203),
+        "stroke_width": 0.3,
+        "fill_color": decimal_color(230, 230, 230),
+    },
+    "vegetation_no_wall": {"fill_color": decimal_color(230, 230, 230)},
+}
+
+
+def draw_base_features(context, base_features, scale_factor, min_lon, min_lat):
+    # TODO: break this up, offset paths
+    base_features.sort(key=lambda d: d["properties"]["z_index"])
+
+    for feature in base_features:
+
+        feature_type = feature["properties"]["type"]
+        style = feature_styles.get(feature_type)
+
+        if not style:
+            print(f"Unknown feature type '{feature_type}', skipping...")
+            continue
+
+        context.save()
+
+        for index, point in enumerate(feature["geometry"]["coordinates"][0]):
+            x = (point[1] - min_lat) * scale_factor
+            y = (point[0] - min_lon) * scale_factor
+
+            if index == 0:
+                context.move_to(x, y)
+            else:
+                context.line_to(x, y)
+
+        context.close_path()
+
+        fill_color = style.get("fill_color")
+        stroke_color = style.get("stroke_color")
+        stroke_width = style.get("stroke_width")
+
+        if fill_color:
+            context.set_source_rgb(*fill_color)
+            if stroke_color and stroke_width:
+                context.fill_preserve()
+            else:
+                context.fill()
+
+        if stroke_color and stroke_width:
+            context.set_line_width(scale_factor * stroke_width)
+            context.set_source_rgb(*stroke_color)
+            context.stroke()
+
+        context.restore()
+
+
 def main():
-    feature_list = get_features()
-    trees, species_list, scale_factor = extract_features(feature_list)
+    feature_list = get_features(current_dir / "data" / "20200530.geojson")
+    base_features = get_features(current_dir / "data" / "base.geojson")
+
+    trees, species_list, scale_factor, min_lon, min_lat = extract_features(feature_list)
 
     svg_path = current_dir / "output" / "example.svg"
     with cairo.SVGSurface(svg_path, 1024, 1024) as surface:
@@ -154,6 +224,9 @@ def main():
         context.set_source_rgb(1, 1, 1)
         context.paint()
         context.restore()
+
+        print("Drawing base map")
+        draw_base_features(context, base_features, scale_factor, min_lon, min_lat)
 
         print(f"Drawing canopy of {len(species_list)} species")
         for species in species_list:
