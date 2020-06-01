fac567c4010f975fba26e7bf7958120a85675cad
Damien Allen <contact@dallen.co>
2020-06-02 00:22:20 +0200

    Updated text function

--- export/draw.py
+++ export/draw.py
@@ -103,35 +103,35 @@ def extract_features(feature_list):
     return (trees, species_list, scale_factor, min_lon, min_lat)
 
 
-def draw_background_outline(context, scale_factor, trees):
-    context.save()
-    context.set_line_width(scale_factor / 5)
+def draw_background_outline(ctx, scale_factor, trees):
+    ctx.save()
+    ctx.set_line_width(scale_factor / 5)
 
     for tree in trees:
-        context.arc(tree["x"], tree["y"], tree["radius"], 0, pi * 2)
-        context.set_source_rgb(*TREE_OUTLINE)
-        context.stroke()
+        ctx.arc(tree["x"], tree["y"], tree["radius"], 0, pi * 2)
+        ctx.set_source_rgb(*TREE_OUTLINE)
+        ctx.stroke()
 
-    context.restore()
+    ctx.restore()
 
 
-def draw_overlay(context, scale_factor, trees):
+def draw_overlay(ctx, scale_factor, trees):
 
     for tree in trees:
-        context.save()
-        context.set_line_width(scale_factor / 10)
-        context.set_dash([scale_factor / 3])
-        context.arc(tree["x"], tree["y"], tree["radius"], 0, pi * 2)
-        context.set_source_rgb(*TREE_OUTLINE)
-        context.stroke()
-        context.restore()
+        ctx.save()
+        ctx.set_line_width(scale_factor / 10)
+        ctx.set_dash([scale_factor / 3])
+        ctx.arc(tree["x"], tree["y"], tree["radius"], 0, pi * 2)
+        ctx.set_source_rgb(*TREE_OUTLINE)
+        ctx.stroke()
+        ctx.restore()
 
-        context.save()
+        ctx.save()
         dot_radius = max(tree["radius"] / 14, 0.001)
-        context.arc(tree["x"], tree["y"], dot_radius, 0, pi * 2)
-        context.set_source_rgba(*COLOR_BLACK, 0.6)
-        context.fill()
-        context.restore()
+        ctx.arc(tree["x"], tree["y"], dot_radius, 0, pi * 2)
+        ctx.set_source_rgba(*COLOR_BLACK, 0.6)
+        ctx.fill()
+        ctx.restore()
 
 
 def fade_white(color, percent):
@@ -141,38 +141,40 @@ def fade_white(color, percent):
     return color + vector * percent
 
 
-def draw_fills(context, scale_factor, trees, species_list):
+def draw_fills(ctx, scale_factor, trees, species_list):
     min_radius = min([species["radius"] for species in species_list])
     max_radius = max([species["radius"] for species in species_list]) + 0.01
     percent = (trees[0]["radius"] - min_radius) / (max_radius - min_radius)
 
     fill_color = fade_white(TREE_FILL, percent)
 
-    context.save()
+    ctx.save()
 
     for tree in trees:
-        context.arc(tree["x"], tree["y"], tree["radius"] - scale_factor / 20, 0, pi * 2)
-        context.set_source_rgba(*fill_color, 1)
-        context.fill()
+        ctx.arc(tree["x"], tree["y"], tree["radius"] - scale_factor / 20, 0, pi * 2)
+        ctx.set_source_rgba(*fill_color, 0.75)
+        ctx.fill()
 
-    context.restore()
+    ctx.restore()
 
 
-def draw_text(context, scale_factor, trees, species_list):
-    min_radius = min([species["radius"] for species in species_list])
-    max_radius = max([species["radius"] for species in species_list]) + 0.01
-    percent = (trees[0]["radius"] - min_radius) / (max_radius - min_radius)
+def draw_text(ctx, scale_factor, trees, species_list):
+    # min_radius = min([species["radius"] for species in species_list])
+    # max_radius = max([species["radius"] for species in species_list]) + 0.01
+    # percent = (trees[0]["radius"] - min_radius) / (max_radius - min_radius)
 
-    fill_color = fade_white(TREE_FILL, percent)
+    # fill_color = fade_white(COLOR_GREY_90, 1 - percent)
 
-    context.save()
+    ctx.save()
+    ctx.set_source_rgb(*COLOR_GREY_70)
+    ctx.set_font_size(1 * scale_factor)
+    ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
 
     for tree in trees:
-        context.arc(tree["x"], tree["y"], tree["radius"] - scale_factor / 20, 0, pi * 2)
-        context.set_source_rgba(*fill_color, 0.7)
-        context.fill()
+        ctx.move_to(tree["x"], tree["y"])
+        ctx.show_text(tree["species"])
 
-    context.restore()
+    ctx.restore()
 
 
 feature_styles = {
@@ -190,7 +192,7 @@ feature_styles = {
 }
 
 
-def draw_base_features(context, base_features, scale_factor, min_lon, min_lat):
+def draw_base_features(ctx, base_features, scale_factor, min_lon, min_lat):
     # TODO: break this up, offset paths
     base_features.sort(key=lambda d: d["properties"]["z_index"])
 
@@ -203,36 +205,36 @@ def draw_base_features(context, base_features, scale_factor, min_lon, min_lat):
             print(f"Unknown feature type '{feature_type}', skipping...")
             continue
 
-        context.save()
+        ctx.save()
 
         for index, point in enumerate(feature["geometry"]["coordinates"][0]):
             x = (point[1] - min_lat) * scale_factor
             y = (point[0] - min_lon) * scale_factor
 
             if index == 0:
-                context.move_to(x, y)
+                ctx.move_to(x, y)
             else:
-                context.line_to(x, y)
+                ctx.line_to(x, y)
 
-        context.close_path()
+        ctx.close_path()
 
         fill_color = style.get("fill_color")
         stroke_color = style.get("stroke_color")
         stroke_width = style.get("stroke_width")
 
         if fill_color:
-            context.set_source_rgb(*fill_color)
+            ctx.set_source_rgb(*fill_color)
             if stroke_color and stroke_width:
-                context.fill_preserve()
+                ctx.fill_preserve()
             else:
-                context.fill()
+                ctx.fill()
 
         if stroke_color and stroke_width:
-            context.set_line_width(scale_factor * stroke_width)
-            context.set_source_rgb(*stroke_color)
-            context.stroke()
+            ctx.set_line_width(scale_factor * stroke_width)
+            ctx.set_source_rgb(*stroke_color)
+            ctx.stroke()
 
-        context.restore()
+        ctx.restore()
 
 
 def generate_pdf(svg_path):
@@ -260,32 +262,32 @@ def main():
     trees, species_list, scale_factor, min_lon, min_lat = extract_features(feature_list)
 
     with cairo.SVGSurface(svg_path, 840, 1200) as surface:
-        context = cairo.Context(surface)
-        context.scale(1200, 1200)
+        ctx = cairo.Context(surface)
+        ctx.scale(1200, 1200)
 
         # Set background
-        context.save()
-        context.set_source_rgb(*COLOR_WHITE)
-        context.paint()
-        context.restore()
+        ctx.save()
+        ctx.set_source_rgb(*COLOR_WHITE)
+        ctx.paint()
+        ctx.restore()
 
         # Position canvas
-        context.translate(*TRANSLATION)
-        context.rotate(ROTATION_ANGLE)
+        ctx.translate(*TRANSLATION)
+        ctx.rotate(ROTATION_ANGLE)
 
         print("Drawing base map")
-        draw_base_features(context, base_features, scale_factor, min_lon, min_lat)
+        draw_base_features(ctx, base_features, scale_factor, min_lon, min_lat)
 
         print(f"Drawing canopy of {len(species_list)} species")
+        draw_background_outline(ctx, scale_factor, trees)
         for species in species_list:
             filtered_trees = [
                 tree for tree in trees if tree["species"] == species["name"]
             ]
-            draw_background_outline(context, scale_factor, filtered_trees)
-            draw_fills(context, scale_factor, filtered_trees, species_list)
-            # draw_text(context, scale_factor, filtered_trees, species_list)
+            draw_fills(ctx, scale_factor, filtered_trees, species_list)
+            # draw_text(ctx, scale_factor, filtered_trees, species_list)
 
-        draw_overlay(context, scale_factor, trees)
+        draw_overlay(ctx, scale_factor, trees)
 
     generate_pdf(svg_path)
 
